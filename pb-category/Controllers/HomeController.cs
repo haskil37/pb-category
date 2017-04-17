1cb29059cd0c92bb22e8e95ab8ed58e3fc0352d9
haskil <haskil37@gmail.com>
2017-04-17 16:14:24 +0300

    second.3

--- pb-category/Controllers/HomeController.cs
+++ pb-category/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace pb_category.Controllers
 {
     public class HomeController : Controller
     {
+        public static int GlobalStep;
         public ActionResult Index()
         {
             return View();
@@ -17,27 +18,33 @@ namespace pb_category.Controllers
         public ActionResult Calculate()
         {
             //Горючие газы
-            Session.Clear();
+            GlobalStep = 1;
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public PartialViewResult CalculateSteps(CalculateModels model)
         {
-            switch (Session["Step"])
+            int CurrentStep = 1;
+            string PartialStep = "Step";
+            float Pmax, P0;
+            if (float.TryParse(model.Pmax, out Pmax))
+                CurrentStep++;
+            if (float.TryParse(model.P0, out P0))
+                CurrentStep++;
+
+            if (Pmax == 0)
+                Pmax = 900;
+            if (P0 == 0)
+                P0 = 101;
+
+
+            if (CurrentStep > GlobalStep)
             {
-                default:
-                    if (!string.IsNullOrEmpty(model.Pmax))
-                        Session["Step"] = 1;
-                    break;
-                case 1:
-                    if (!string.IsNullOrEmpty(model.P0))
-                        Session["Step"] = 2;
-                    else
-                        Session["Step"] = 0;
-                    break;
+                GlobalStep = CurrentStep;
+                PartialStep += CurrentStep;
             }
-            return PartialView();
+            return PartialView(PartialStep);
         }
     }
 }
--- pb-category/Models/CalculateModels.cs
+++ pb-category/Models/CalculateModels.cs
@@ -9,7 +9,18 @@ namespace pb_category.Models
     {
         public string Pmax { get; set; }
         public string P0 { get; set; }
-
+        public string Vcb { get; set; }
+        public string Kh { get; set; }
+        public string Z { get; set; }
+        public string Rogp { get; set; }
+        public string M { get; set; }
+        public string Tp { get; set; }
+        public string V0 { get; set; }
+        public string Cct { get; set; }
+        public string Beta { get; set; }
+        public string Nc { get; set; }
+        public string Nh { get; set; }
+        public string Nx { get; set; }
+        public string No { get; set; }
     }
-
 }
